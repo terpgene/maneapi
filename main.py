c520ee83ea69e5f155b3d392fa331b1faef7fa25
Gene Essel <eugene2essel@gmail.com>
2021-09-13 00:50:18 -0400

    Multiple path and query parameters

--- main.py
+++ main.py
@@ -72,3 +72,17 @@ async def read_optional(optional_id: str, q: Optional[str] = None, short: bool =
             {"description": "This is an amazing item that has a long description"}
         )
     return optional
+
+# Multiple path and query parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update( 
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
