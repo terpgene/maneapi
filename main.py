73a353a446049d9c0c08888f7744b7ecde27aa12
Gene Essel <eugene2essel@gmail.com>
2021-09-13 00:37:36 -0400

    Query parameter type conversion

--- main.py
+++ main.py
@@ -59,3 +59,16 @@ async def read_opt(opts_id: str, q: Optional[str] = None):
     if q:
         return {"opts_id": opts_id, "q": q}
     return {"opts_id": opts_id}
+
+
+# Query parameter type conversion - you can declare bool types and they will be converted
+@app.get("/optional/{optional_id}")
+async def read_optional(optional_id: str, q: Optional[str] = None, short: bool = False):
+    optional = {"optonal_id": optional_id}
+    if q:
+        optional.update({"q": q})
+    if not short:
+        optional.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return optional
